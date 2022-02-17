d4f592016b4a8bb9a25889efa2353c18419742e3
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-02-17 21:27:16 +0100

    Created endpoint for updating user information

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const models = require('../database/models')
 const token = require("../helpers/token");
+const {where} = require("sequelize");
+const findUserByEmail = require("../helpers/findUserByEmail");
 
 router.get("/", async (req, res) => {
     const id = token.getIdFromRefreshToken(req.cookies)
@@ -28,5 +30,52 @@ router.get("/", async (req, res) => {
 
     return res.json(user);
 })
+
+router.put("/update-information", async (req, res) => {
+    const { firstName, lastName, email } = req.body;
+    const id = token.getIdFromRefreshToken(req.cookies)
+
+    if (!id) {
+        return res.status(401).json({
+            message: "Unauthorized!"
+        })
+    }
+
+    // Prevents assigning used email by another user to the current user
+
+    try {
+        const user = await findUserByEmail(email);
+
+        if (user && user.id !== id) {
+            return res.status(400).json({
+                message: "Email is already in use!"
+            })
+        }
+    } catch (err) {
+        console.log(err)
+    }
+
+    try {
+        const user = await models.User.findByPk(id);
+
+        const updatedUser = await user.update({
+            firstName: firstName,
+            lastName: lastName,
+            email: email
+        })
+
+        const response = {
+            firstName: updatedUser.firstName,
+            lastName: updatedUser.lastName,
+            email: updatedUser.email
+        }
+
+        return res.json(response)
+
+    } catch (err) {
+        console.log(err)
+    }
+})
+
 
 module.exports = router;
