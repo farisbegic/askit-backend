ba1fd49d6d81dbe9382cf5dd5f33c0f1046e5a0e
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-02-18 20:48:28 +0100

    Implemented update and delete answer endpoints

--- routes/answer.js
+++ routes/answer.js
@@ -30,7 +30,71 @@ router.post("/", async (req, res) => {
             });
         }
     } catch (err) {
-        return res.status(500).json({
+        return gres.status(500).json({
+            message: err.message,
+        });
+    }
+})
+
+router.put("/", async (req, res) => {
+    const { answerId, description } = req.body;
+    const id = token.getIdFromRefreshToken(req.cookies)
+
+    try {
+        const answer = await models.Answer.findByPk(answerId);
+
+        if (!answer) {
+            return res.status(400).json({
+                message: "Answer doesn't exist"
+            })
+        }
+
+        if (answer.user !== id) {
+            return res.status(401).json({
+                message: "You are not authorized to update this answer",
+            });
+        }
+
+        await answer.update({
+            description: description
+        })
+
+        return res.status(200).json({
+            message: "You have successfully updated an answer",
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+        });
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    const { id: answerId } = req.params;
+    const id = token.getIdFromRefreshToken(req.cookies)
+
+    try {
+        const answer = await models.Answer.findByPk(answerId);
+
+        if (!answer) {
+            return res.status(400).json({
+                message: "Answer doesn't exist"
+            })
+        }
+
+        if (answer.user !== id) {
+            return res.status(401).json({
+                message: "You are not authorized to delete this answer",
+            });
+        }
+
+        await answer.destroy();
+
+        return res.status(200).json({
+            message: "You have successfully deleted an answer",
+        });
+    } catch (err) {
+        res.status(500).json({
             message: err.message,
         });
     }
