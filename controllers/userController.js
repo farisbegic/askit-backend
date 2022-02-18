be72fa0873d422a345faaf8ee74d8c72501d6578
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-02-18 21:35:14 +0100

    Separated logic from user enpoint

---
+++ controllers/userController.js
@@ -0,0 +1,147 @@
+const models = require("../database/models");
+const token = require("../helpers/token");
+const findUserByEmail = require("../helpers/findUserByEmail");
+const bcrypt = require("bcryptjs");
+const {Sequelize} = require("sequelize");
+
+const getUser = async (req, res) => {
+    const id = token.getIdFromRefreshToken(req.cookies)
+
+    if (!id) {
+        return res.status(401).json({
+            message: "Unauthorized!"
+        })
+    }
+
+    try {
+        const user = await models.User
+            .findOne({
+                where: {
+                    id: id
+                },
+                attributes: ['id', 'firstName', 'lastName', 'email']
+            })
+
+        if (!user) {
+            return res.status(500).json({
+                message: "User not found."
+            })
+        }
+
+        return res.json(user);
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+const updateUser = async (req, res) => {
+    const { firstName, lastName, email } = req.body;
+    const id = token.getIdFromRefreshToken(req.cookies)
+
+    if (!id) {
+        return res.status(401).json({
+            message: "Unauthorized!"
+        })
+    }
+
+    // Prevents assigning used email by another user to the current user
+
+    try {
+        const user = await findUserByEmail(email);
+
+        if (user && user.id !== id) {
+            return res.status(400).json({
+                message: "Email is already in use!"
+            })
+        }
+    } catch (err) {
+        console.log(err)
+    }
+
+    try {
+        const user = await models.User.findByPk(id);
+
+        const updatedUser = await user.update({
+            firstName: firstName,
+            lastName: lastName,
+            email: email
+        })
+
+        const response = {
+            firstName: updatedUser.firstName,
+            lastName: updatedUser.lastName,
+            email: updatedUser.email
+        }
+
+        return res.json(response)
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const updatePassword = async (req, res) => {
+    const { password, confirmPassword } = req.body;
+    const id = token.getIdFromRefreshToken(req.cookies)
+
+    if (password !== confirmPassword) {
+        return res.status(400).json({
+            message: "Passwords do not match."
+        })
+    }
+
+    if (!id) {
+        return res.status(401).json({
+            message: "Unauthorized!"
+        })
+    }
+
+    try {
+        const user = await models.User.findByPk(id);
+
+        await user.update({
+            password: bcrypt.hashSync(password, 8)
+        })
+
+        res.json({
+            message: "Password has been updated successfully"
+        })
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const getUsersWithMostAnswers = async (req, res) => {
+    try {
+        const users = await models.User.findAll({
+            attributes: [
+                "id",
+                "firstName",
+                "lastName",
+                [Sequelize.fn("COUNT", Sequelize.col("Answers.id")), "count"],
+            ],
+            include: {
+                model: models.Answer,
+                attributes: []
+            },
+            group: ["User.id"],
+            order: [['"count"', "DESC"]],
+        })
+
+        return res.json(users)
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+module.exports = {
+    getUser,
+    updateUser,
+    updatePassword,
+    getUsersWithMostAnswers
+}
--- routes/user.js
+++ routes/user.js
@@ -1,138 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const models = require('../database/models')
-const token = require("../helpers/token");
-const findUserByEmail = require("../helpers/findUserByEmail");
-const bcrypt = require("bcryptjs");
-const {Sequelize} = require("sequelize");
+const userController = require("../controllers/userController")
 
-router.get("/", async (req, res) => {
-    const id = token.getIdFromRefreshToken(req.cookies)
+// Fetch user information
+router.get("/", userController.getUser)
 
-    if (!id) {
-        return res.status(401).json({
-            message: "Unauthorized!"
-        })
-    }
+// Update user information
+router.put("/update-information", userController.updateUser)
 
-    const user = await models.User
-        .findOne({
-            where: {
-                id: id
-            },
-            attributes: ['id', 'firstName', 'lastName', 'email']
-        })
+// Update user password
+router.post("/update-password", userController.updatePassword)
 
-    if (!user) {
-        return res.status(500).json({
-            message: err.message
-        })
-    }
-
-    return res.json(user);
-})
-
-router.put("/update-information", async (req, res) => {
-    const { firstName, lastName, email } = req.body;
-    const id = token.getIdFromRefreshToken(req.cookies)
-
-    if (!id) {
-        return res.status(401).json({
-            message: "Unauthorized!"
-        })
-    }
-
-    // Prevents assigning used email by another user to the current user
-
-    try {
-        const user = await findUserByEmail(email);
-
-        if (user && user.id !== id) {
-            return res.status(400).json({
-                message: "Email is already in use!"
-            })
-        }
-    } catch (err) {
-        console.log(err)
-    }
-
-    try {
-        const user = await models.User.findByPk(id);
-
-        const updatedUser = await user.update({
-            firstName: firstName,
-            lastName: lastName,
-            email: email
-        })
-
-        const response = {
-            firstName: updatedUser.firstName,
-            lastName: updatedUser.lastName,
-            email: updatedUser.email
-        }
-
-        return res.json(response)
-
-    } catch (err) {
-        console.log(err)
-    }
-})
-
-router.post("/update-password", async (req, res) => {
-    const { password, confirmPassword } = req.body;
-    const id = token.getIdFromRefreshToken(req.cookies)
-
-    if (password !== confirmPassword) {
-        return res.status(400).json({
-            message: "Passwords do not match."
-        })
-    }
-
-    if (!id) {
-        return res.status(401).json({
-            message: "Unauthorized!"
-        })
-    }
-
-    try {
-        const user = await models.User.findByPk(id);
-
-        await user.update({
-            password: bcrypt.hashSync(password, 8)
-        })
-
-        res.json({
-            message: "Password has been updated successfully"
-        })
-
-    } catch (err) {
-        console.log(err)
-    }
-})
-
-router.get("/most-answers", async (req, res) => {
-    try {
-        const users = await models.User.findAll({
-            attributes: [
-                "id",
-                "firstName",
-                "lastName",
-                [Sequelize.fn("COUNT", Sequelize.col("Answers.id")), "count"],
-            ],
-            include: {
-                model: models.Answer,
-                attributes: []
-            },
-            group: ["User.id"],
-            order: [['"count"', "DESC"]],
-        })
-
-        return res.json(users)
-    } catch (err) {
-        return res.status(500).json({
-            message: err.message
-        })
-    }
-})
+// Fetch users with most answers (Most active users)
+router.get("/most-answers", userController.getUsersWithMostAnswers)
 
 module.exports = router;
