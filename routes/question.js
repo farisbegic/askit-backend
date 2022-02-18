26535672ed0cfb7116b33486e699ba89f78b3900
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-02-18 19:04:58 +0100

    Implemented question with all details endpoint

--- routes/question.js
+++ routes/question.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const models = require('../database/models')
 const token = require("../helpers/token");
+const {Sequelize} = require("sequelize");
 
 router.get("/my-questions",  async (req, res) => {
     const id = token.getIdFromRefreshToken(req.cookies)
@@ -29,7 +30,123 @@ router.get("/my-questions",  async (req, res) => {
 
         return res.json(questions);
     } catch (err) {
-        res.status(500).json({
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+})
+
+router.get("/:id", async (req, res) => {
+    const { id: questionId } = req.params;
+    const id = token.getIdFromRefreshToken(req.cookies)
+
+    try {
+        const question = await models.Question.findOne({
+            where: {
+                id: questionId,
+            },
+            attributes: [
+                "id",
+                "description",
+                "createdAt",
+                [
+                    Sequelize.literal(`(
+                SELECT COUNT(*)
+                FROM "QuestionRatings"
+                WHERE "question" = ${questionId} AND "isLike" = true
+                )`),
+                    "likes",
+                ],
+                [
+                    Sequelize.literal(`(
+                SELECT COUNT(*)
+                FROM "QuestionRatings"
+                WHERE "question" = ${questionId} AND "isLike" = false
+                )`),
+                    "dislikes",
+                ],
+                [
+                    Sequelize.literal(`(
+                SELECT COUNT(*)
+                FROM "QuestionRatings"
+                WHERE "question" = ${questionId} AND "user" = ${id} AND "isLike" = true
+                )`),
+                    "hasLiked",
+                ],
+                [
+                    Sequelize.literal(`(
+                SELECT COUNT(*)
+                FROM "QuestionRatings"
+                WHERE "question" = ${questionId} AND "user" = ${id} AND "isLike" = false
+                )`),
+                    "hasDisliked",
+                ],
+            ],
+            include: [
+                {
+                    model: models.Answer,
+                    attributes: [
+                        "id",
+                        "description",
+                        "createdAt",
+                        [
+                            Sequelize.literal(`(
+                SELECT COUNT(*)
+                FROM "AnswerRatings"
+                WHERE "answer" = "Answers".id AND "isLike" = true
+                )`),
+                            "likes",
+                        ],
+                        [
+                            Sequelize.literal(`(
+                SELECT COUNT(*)
+                FROM "AnswerRatings"
+                WHERE "answer" = "Answers".id AND "isLike" = false
+                )`),
+                            "dislikes",
+                        ],
+                        [
+                            Sequelize.literal(`(
+                SELECT COUNT(*)
+                FROM "AnswerRatings"
+                WHERE "answer" = "Answers".id AND "user" = ${id} AND "isLike" = true
+                )`),
+                            "hasLiked",
+                        ],
+                        [
+                            Sequelize.literal(`(
+                SELECT COUNT(*)
+                FROM "AnswerRatings"
+                WHERE "answer" = "Answers".id AND "user" = ${id} AND "isLike" = false
+                )`),
+                            "hasDisliked",
+                        ],
+                    ],
+                    include: [
+                        {
+                            model: models.User,
+                            attributes: ["id", "firstName", "lastName"],
+                        }
+                    ],
+                },
+                {
+                    model: models.User,
+                    attributes: ["id", "firstName", "lastName"],
+                },
+            ],
+            order: [[{ model: models.Answer }, "createdAt", "ASC"]],
+        });
+
+        if (!question) {
+            return res.status(400).json({
+                message: "Question doesn't exist."
+            })
+        }
+
+        return res.json(question);
+
+    } catch (err) {
+        return res.status(500).json({
             message: err.message
         })
     }
