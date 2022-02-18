0c4bca3c4603b517ba26eec3300950b81d0fcb26
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-02-18 22:11:02 +0100

    Separated logic from answer endpoint

---
+++ controllers/answerController.js
@@ -0,0 +1,105 @@
+const token = require("../helpers/token");
+const models = require("../database/models");
+
+const saveAnswer = async (req, res) => {
+    const { description, questionId } = req.body
+    const id = token.getIdFromRefreshToken(req.cookies)
+
+    try {
+        const question = await models.Question.findByPk(questionId);
+
+        if (!question) {
+            return res.status(400).json({
+                message: "Question doesn't exist"
+            })
+        }
+
+        const answer = await models.Answer.create({
+            description: description,
+            question: questionId,
+            user: id,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        })
+
+        if (answer) {
+            return res.json({
+                message: "You have successfully saved an answer",
+            });
+        }
+    } catch (err) {
+        return gres.status(500).json({
+            message: err.message,
+        });
+    }
+}
+
+const updateAnswer = async (req, res) => {
+    const { answerId, description } = req.body;
+    const id = token.getIdFromRefreshToken(req.cookies)
+
+    try {
+        const answer = await models.Answer.findByPk(answerId);
+
+        if (!answer) {
+            return res.status(400).json({
+                message: "Answer doesn't exist"
+            })
+        }
+
+        if (answer.user !== id) {
+            return res.status(401).json({
+                message: "You are not authorized to update this answer",
+            });
+        }
+
+        await answer.update({
+            description: description
+        })
+
+        return res.status(200).json({
+            message: "You have successfully updated an answer",
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+        });
+    }
+}
+
+const deleteAnswer = async (req, res) => {
+    const { id: answerId } = req.params;
+    const id = token.getIdFromRefreshToken(req.cookies)
+
+    try {
+        const answer = await models.Answer.findByPk(answerId);
+
+        if (!answer) {
+            return res.status(400).json({
+                message: "Answer doesn't exist"
+            })
+        }
+
+        if (answer.user !== id) {
+            return res.status(401).json({
+                message: "You are not authorized to delete this answer",
+            });
+        }
+
+        await answer.destroy();
+
+        return res.status(200).json({
+            message: "You have successfully deleted an answer",
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+        });
+    }
+}
+
+module.exports = {
+    saveAnswer,
+    updateAnswer,
+    deleteAnswer
+}
--- routes/answer.js
+++ routes/answer.js
@@ -2,102 +2,14 @@ const express = require("express");
 const router = express.Router();
 const models = require('../database/models')
 const token = require("../helpers/token");
+const answerController = require("../controllers/answerController")
 
-router.post("/", async (req, res) => {
-    const { description, questionId } = req.body
-    const id = token.getIdFromRefreshToken(req.cookies)
+// Save an answer
+router.post("/", answerController.saveAnswer)
 
-    try {
-        const question = await models.Question.findByPk(questionId);
+// Update an answer
+router.put("/", answerController.updateAnswer)
 
-        if (!question) {
-            return res.status(400).json({
-                message: "Question doesn't exist"
-            })
-        }
-
-        const answer = await models.Answer.create({
-            description: description,
-            question: questionId,
-            user: id,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        })
-
-        if (answer) {
-            return res.json({
-                message: "You have successfully saved an answer",
-            });
-        }
-    } catch (err) {
-        return gres.status(500).json({
-            message: err.message,
-        });
-    }
-})
-
-router.put("/", async (req, res) => {
-    const { answerId, description } = req.body;
-    const id = token.getIdFromRefreshToken(req.cookies)
-
-    try {
-        const answer = await models.Answer.findByPk(answerId);
-
-        if (!answer) {
-            return res.status(400).json({
-                message: "Answer doesn't exist"
-            })
-        }
-
-        if (answer.user !== id) {
-            return res.status(401).json({
-                message: "You are not authorized to update this answer",
-            });
-        }
-
-        await answer.update({
-            description: description
-        })
-
-        return res.status(200).json({
-            message: "You have successfully updated an answer",
-        });
-    } catch (err) {
-        res.status(500).json({
-            message: err.message,
-        });
-    }
-})
-
-router.delete("/:id", async (req, res) => {
-    const { id: answerId } = req.params;
-    const id = token.getIdFromRefreshToken(req.cookies)
-
-    try {
-        const answer = await models.Answer.findByPk(answerId);
-
-        if (!answer) {
-            return res.status(400).json({
-                message: "Answer doesn't exist"
-            })
-        }
-
-        if (answer.user !== id) {
-            return res.status(401).json({
-                message: "You are not authorized to delete this answer",
-            });
-        }
-
-        await answer.destroy();
-
-        return res.status(200).json({
-            message: "You have successfully deleted an answer",
-        });
-    } catch (err) {
-        res.status(500).json({
-            message: err.message,
-        });
-    }
-})
+router.delete("/:id", answerController.deleteAnswer)
 
 module.exports = router
