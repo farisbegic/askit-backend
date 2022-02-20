e1dbf6bab90913a167fb574d3dd7f292ac5d8d37
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-02-20 20:39:28 +0100

    New commit

--- database/config/config.js
+++ database/config/config.js
@@ -1,23 +1,21 @@
-require('dotenv').config()
-
 module.exports = {
   "development": {
-    "username": process.env.DBUSERNAME,
-    "password": process.env.DBPASSWORD,
+    "username": "postgres",
+    "password": "postgres",
     "database": "ask.it",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
-    "username": process.env.DBUSERNAME,
-    "password": process.env.DBPASSWORD,
+    "username": "postgres",
+    "password": "postgres",
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DBUSERNAME,
-    "password": process.env.DBPASSWORD,
+    "username": "postgres",
+    "password": "postgres",
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "postgres"
