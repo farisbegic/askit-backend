05fff3ce0303b116e5c221ffc5a6c4da7e064d9a
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-02-21 10:15:36 +0100

    Made things suitable for dev v2

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -35,7 +35,7 @@ const login = async (req, res) => {
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "none"
+            sameSite: process.env.NODE_ENV === "production" ? "none" : ""
         });
 
         res.json({
