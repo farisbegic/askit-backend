2ac77a7225b9ed845ea457fee5c380791a9f9794
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-02-23 19:06:25 +0100

    Cookie settings

--- index.js
+++ index.js
@@ -30,21 +30,10 @@ io.on("connection", (socket) => {
 app.enable('trust proxy');
 app.set("socketio", io)
 
-const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://ask-it-frontend.herokuapp.com/']
-const corsOptions = {
-    origin: function (origin, callback) {
-        console.log("** Origin of request " + origin)
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            console.log("Origin acceptable")
-            callback(null, true)
-        } else {
-            console.log("Origin rejected")
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
+app.use(cors({
+    origin: true,
     credentials: true
-}
-app.use(cors(corsOptions))
+}));
 
 app.use(cookieParser())
 
